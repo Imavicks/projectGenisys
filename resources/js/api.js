19f0ecc561d64d3df5d18b8b3e39a4bd23b4f9a4
Imavicks <victor.imazobi@gmail.com>
2023-08-29 22:40:00 +0100

    update api.js

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,4 +1,9 @@
-const url = 'http://102.36.176.228:4444/products/get/6';
+// Import the functions from script.js
+import {
+  changeGridA, changeGridBtxt, addBorderB, selectImage, selectWidth, selectSize, addToCart,
+} from './script.js';
+
+const url = 'http://102.36.176.228:4444/products/get/2';
 const options = {
   method: 'GET',
   headers: {
@@ -9,14 +14,15 @@ const options = {
 fetch(url, options)
   .then((response) => response.json())
   .then((data) => {
+    // console.log(data);
     // Update product name
     document.getElementById('productName').innerText = data.productName;
 
     // Update product details
-    document.getElementById('productPrice').innerText = `&#8358;${data.discountedPrice}`;
-    document.getElementById('discountPercentage').innerText = `${((data.discountedPrice / data.productPrice) * 100).toFixed(0)}%`;
-    document.getElementById('originalPrice').innerText = `&#8358;${data.productPrice}`;
-    document.getElementById('colorName').innerText = data.productColors[0].color;
+    document.getElementById('productPrice').innerHTML = `&#8358;${data.discountedPrice}`;
+    document.getElementById('discountPercentage').innerHTML = `${((data.discountedPrice / data.productPrice) * 100).toFixed(0)}%`;
+    document.getElementById('originalPrice').innerHTML = `&#8358;${data.productPrice}`;
+    document.getElementById('colorName').innerHTML = data.productColors[0].color;
 
     // Update color images
     const colorImagesContainer = document.getElementById('colorImages');
@@ -25,12 +31,10 @@ fetch(url, options)
       img.src = color.image;
       img.alt = 'Product Image';
       img.onclick = () => {
-        /*
         changeGridA(color.id);
         changeGridBtxt(color.id);
         addBorderB(img);
         selectImage(img);
-        */
       };
       colorImagesContainer.appendChild(img);
     });
@@ -41,12 +45,22 @@ fetch(url, options)
       const button = document.createElement('button');
       button.type = 'button';
       button.innerText = size;
-      // button.onclick = () => selectSize(button);
+      button.onclick = () => selectSize(button);
       sizeButtonsContainer.appendChild(button);
     });
+
+    // Update width buttons and attach event listeners
+    const widthButtons = document.querySelectorAll('.select-width button');
+    widthButtons.forEach((button) => {
+      button.addEventListener('click', () => selectWidth(button));
+    });
+
+    // Attach event listener to 'Add to Cart' button
+    const addToCartButton = document.querySelector('.add-cart button');
+    addToCartButton.addEventListener('click', addToCart);
   });
+
 /*
   .catch((error) => {
     console.log('Error', error);
-  });
-*/
+  }); */
